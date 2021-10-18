08ce708aff713df77a426c7913defa9a89cfc04d
SnowDango <xdi.grand.you.winner@gmail.com>
2021-10-18 11:32:59 +0900

    Add HandManagerL files

---
+++ Assets/Oculus/HandManager/FingerPitchs.cs
@@ -0,0 +1,11 @@
+﻿namespace Oculus.handManager 
+{
+    public enum FingerPitchs
+    {
+        Thumb = 0,
+        Index = 1,
+        Middle = 2,
+        Ring = 3,
+        Pinky = 4
+    }
+}
---
+++ Assets/Oculus/HandManager/HandManagerL.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Oculus.handManager
+{
+    public class HandManagerL : MonoBehaviour
+    {
+        [SerializeField, Tooltip("ピンチ状態取得用OVRHand")]
+        private OVRHand _ovrHand;
+        
+        private void Update() {
+            // pitch finger list 
+            ArrayList fingerPitchList = new ArrayList( 
+                capacity: 5
+            ) {
+                _ovrHand.GetFingerIsPinching(OVRHand.HandFinger.Thumb),
+                _ovrHand.GetFingerIsPinching(OVRHand.HandFinger.Index),
+                _ovrHand.GetFingerIsPinching(OVRHand.HandFinger.Middle),
+                _ovrHand.GetFingerIsPinching(OVRHand.HandFinger.Ring),
+                _ovrHand.GetFingerIsPinching(OVRHand.HandFinger.Pinky)
+            };
+
+        }
+    }
+}
